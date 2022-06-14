417b04736598e8c90af58b1907101baf02d2afb5
lauany <slauany2004@gmail.com>
2022-06-14 20:17:54 -0300

    mundança no portifólio

---
+++ event.js
@@ -0,0 +1,185 @@
+
+let css = document.querySelector('#css');
+let textCss = document.querySelector('.css');
+//event hover
+css.addEventListener('mouseenter', function(){
+    textCss.style.display = 'flex';
+    css.style.backgroundColor = 'white';
+});
+css.addEventListener('mouseout', function(){
+textCss.style.display = 'none';
+css.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+//event click
+css.addEventListener('click', function(){
+if(textCss.style.display === 'none'){
+    textCss.style.display = 'flex';
+    css.style.backgroundColor = 'white';
+
+} else{
+    textCss.style.display = 'none';
+    css.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+}
+});
+
+ 
+
+
+//event hover
+let figma = document.querySelector('#figma');
+let textFigma = document.querySelector('.figma');
+figma.addEventListener('mouseenter', function(){
+        textFigma.style.display = 'flex';
+        figma.style.backgroundColor = 'white';
+});
+figma.addEventListener('mouseout', function(){
+    textFigma.style.display = 'none';
+    figma.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+
+//event click
+figma.addEventListener('click', function(){
+    if(textFigma.style.display === 'none'){
+        textFigma.style.display = 'flex';
+        figma.style.backgroundColor = 'white';
+
+    } else{
+        textFigma.style.display = 'none';
+        figma.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
+
+//git
+let git= document.querySelector('#git');
+let textGit= document.querySelector('.git');
+
+git.addEventListener('mouseenter', function(){
+        textGit.style.display = 'flex';
+        git.style.backgroundColor = 'white';
+});
+git.addEventListener('mouseout', function(){
+    textGit.style.display = 'none';
+    git.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+git.addEventListener('click', function(){
+    if(textGit.style.display === 'none'){
+        textGit.style.display = 'flex';
+        git.style.backgroundColor = 'white';
+
+    } else{
+        textGit.style.display = 'none';
+        git.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
+
+//github
+let github = document.querySelector('#github');
+let textGithub = document.querySelector('.github');
+github.addEventListener('mouseenter', function(){
+    textGithub.style.display = 'flex';
+    github.style.backgroundColor = 'white';
+});
+github.addEventListener('mouseout', function(){
+textGithub.style.display = 'none';
+github.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+
+github.addEventListener('click', function(){
+    if(textGithub.style.display === 'none'){
+        textGithub.style.display = 'flex';
+     github.style.backgroundColor = 'white';
+
+    } else{
+        textGithub.style.display = 'none';
+     github.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
+
+//html
+let html = document.querySelector('#html');
+let textHtml = document.querySelector('.html');
+html.addEventListener('mouseenter', function(){
+    textHtml.style.display = 'flex';
+    html.style.backgroundColor = 'white';
+});
+html.addEventListener('mouseout', function(){
+textHtml.style.display = 'none';
+html.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+html.addEventListener('click', function(){
+    if(textHtml.style.display === 'none'){
+        textHtml.style.display = 'flex';
+        html.style.backgroundColor = 'white';
+
+    } else{
+        textHtml.style.display = 'none';
+        html.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
+
+
+//javascript
+let js = document.querySelector('#js');
+let textJs = document.querySelector('.js');
+js.addEventListener('mouseenter', function(){
+    textJs.style.display = 'flex';
+    js.style.backgroundColor = 'white';
+});
+js.addEventListener('mouseout', function(){
+textJs.style.display = 'none';
+js.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+js.addEventListener('click', function(){
+    if(textJs.style.display === 'none'){
+        textJs.style.display = 'flex';
+        js.style.backgroundColor = 'white';
+
+    } else{
+        textJs.style.display = 'none';
+        js.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
+
+//node.js
+let node = document.querySelector('#node');
+let textNode = document.querySelector('.node');
+node.addEventListener('mouseenter', function(){
+    textNode.style.display = 'flex';
+    node.style.backgroundColor = 'white';
+});
+node.addEventListener('mouseout', function(){
+textNode.style.display = 'none';
+node.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+node.addEventListener('click', function(){
+    if(textNode.style.display === 'none'){
+        textNode.style.display = 'flex';
+        node.style.backgroundColor = 'white';
+
+    } else{
+        textNode.style.display = 'none';
+        node.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
+
+//visual studio code
+let vsc = document.querySelector('#vsc');
+let textVsc = document.querySelector('.vsc');
+vsc.addEventListener('mouseenter', function(){
+    textVsc.style.display = 'flex';
+    vsc.style.backgroundColor = 'white';
+});
+vsc.addEventListener('mouseout', function(){
+textVsc.style.display = 'none';
+vsc.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+});
+vsc.addEventListener('click', function(){
+    if(textVsc.style.display === 'none'){
+        textVsc.style.display = 'flex';
+        vsc.style.backgroundColor = 'white';
+
+    } else{
+        textVsc.style.display = 'none';
+        vsc.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    }
+});
